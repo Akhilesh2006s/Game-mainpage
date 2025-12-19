8007726f3bec8ae78222e5e61d836238810ece6c
Akhilesh2006s <akhileshsamayamanthula@gmail.com>
2025-12-19 22:31:12 +0530

    ok

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: ["class"],
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}', '../Landing page/**/*.{ts,tsx}'],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   prefix: "",
   theme: {
     container: {
